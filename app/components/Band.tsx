2455b67d250d6091307d46d8858dc815260128ca
Charles Delfau <charlesdelfau@gmail.com>
2024-10-07 06:39:50 +0200

    bandsAnimation

--- app/components/Band.tsx
+++ app/components/Band.tsx
@@ -13,19 +13,21 @@ interface BandProps {
 function Band({ first }: BandProps) {
     return (
         <div>
-            <div className={`absolute w-[10000px] h-[120px] flex justify-around items-center text-[var(--background)] font-black text-5xl z-100 rotate-[4deg] bg-[var(--fontColorDark)] left-1/2 -translate-x-[50%] -z-10 ${first ? 'top-[400px]' : ''}`}>
-                {
-                    Array(9).fill(0).map(() => (
-                        <>
-                            <p>BITE NOW</p>
-                            <BurgerBubble src={Chicken} small />
-                            <p>BITE NOW</p>
-                            <BurgerBubble src={Bacon} small />
-                            <p>BITE NOW</p>
-                            <BurgerBubble src={Double} small />
-                        </>
-                    ))
-                }
+            <div className={`absolute w-[6000px] h-[120px] text-[var(--background)] font-black text-5xl z-100 rotate-[4deg] bg-[var(--fontColorDark)] left-1/2 -translate-x-[50%] -z-10 ${first ? 'top-[400px]' : ''}`}>
+                <div className="band-container">
+                    {
+                        Array(9).fill(0).map((_, index) => (
+                            <div className='band-child absolute left-[90%] flex justify-around items-center' key={index}>
+                                <p>BITE NOW</p>
+                                <BurgerBubble src={Chicken} small />
+                                <p>BITE NOW</p>
+                                <BurgerBubble src={Bacon} small />
+                                <p>BITE NOW</p>
+                                <BurgerBubble src={Double} small />
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
             <div className={`absolute w-[10000px] h-[120px] flex justify-around items-center text-[var(--background)] font-black text-5xl z-100 rotate-[-4deg] bg-[var(--fontColor)] left-1/2 -translate-x-[50%] z-10 ${first ? 'top-[400px]' : ''}`}>
                 {
